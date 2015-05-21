53685a99301586edc585f69d64723cd65fe60291
Javo Rosas <javier.rosasb@gmail.com>
2015-05-21 12:39:06 -0700

    Initial Commit

---
+++ GeoReminders/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  GeoReminders
+//
+//  Created by Javier Rosas on 5/21/15.
+//  Copyright (c) 2015 Javier Rosas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ GeoReminders/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  GeoReminders
+//
+//  Created by Javier Rosas on 5/21/15.
+//  Copyright (c) 2015 Javier Rosas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
