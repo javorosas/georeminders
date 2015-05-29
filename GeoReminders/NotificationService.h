76b7250104d71ee3f76f4074d8fcfc971d21f5b0
Javo Rosas <javier.rosasb@gmail.com>
2015-05-29 10:00:38 -0700

    add uuid field

---
+++ GeoReminders/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  GeoReminders
+//
+//  Created by Javier Rosas on 5/28/15.
+//  Copyright (c) 2015 Javier Rosas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NotificationService : NSObject
+
+@end
---
+++ GeoReminders/NotificationService.m
@@ -0,0 +1,13 @@
+//
+//  NotificationService.m
+//  GeoReminders
+//
+//  Created by Javier Rosas on 5/28/15.
+//  Copyright (c) 2015 Javier Rosas. All rights reserved.
+//
+
+#import "NotificationService.h"
+
+@implementation NotificationService
+
+@end
